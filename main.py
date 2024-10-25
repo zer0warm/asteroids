8dc42ff74cbfc75a9ae56b74abd2806391880851
zer0warm <dqhieu.2016@gmail.com>
2024-10-25 18:26:12 +0700

    add: Draw a player on screen
    Draw a player that is internally a circle (by subclassing CircleShape)
    but visually a triangle (by computing through triangle()).
    
    - new(player.py):
      - define Player class, subclassing CircleShape
      - has its own rotation value
      - compute the visual triangle, using the rotation value
      - code to draw its triangle
    - update(main.py): code to instantiate and draw a Player
    - update(constants.py): PLAYER_WIDTH constant

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
 
---
+++ player.py
@@ -0,0 +1,24 @@
+import pygame
+
+from constants import *
+from circleshape import CircleShape
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def triangle(self):
+        """Return positions of 3 triangle vertices"""
+        forward = pygame.Vector2(0, -1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+
+        return [a, b, c]
+
+    def draw(self, screen):
+        """Draw a white player"""
+        pygame.draw.polygon(screen, (0xff, 0xff, 0xff), self.triangle(), 2)
