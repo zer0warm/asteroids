d8adf708b3c08b3d93731169abfcc12b2d2fe11b
zer0warm <dqhieu.2016@gmail.com>
2024-10-25 16:47:27 +0700

    add: first actual pygame code
    - update(main.py):
      - initialize pygame and set parameters
      - make event loop
      - make 'close' button on the UI works
    - new(constants.py): shared constants of the project

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pygame
 
+from constants import *
+
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == '__main__':
     main()
